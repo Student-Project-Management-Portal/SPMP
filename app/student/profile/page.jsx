5432e7c436d7f99e39b0c6e98631b68a7d6aa496
Dev Chauhan <devccode8@gmail.com>
2023-05-07 09:01:21 +0530

    project card

---
+++ app/student/profile/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Project from "./project";
+
+const layout = () => {
+  return (
+    <div id="profile">
+      <Project/>
+    </div>
+  );
+};
+
+export default layout;
